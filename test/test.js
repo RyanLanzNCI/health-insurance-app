b357354e1e3bc7047783caac712ee93d0563fe99
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-04-10 13:42:46 +0100

    unit test #1

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var insuranceCalculator = require("../modules/utils")
+
+describe('insuranceCalculator', function() {
+    describe('Creates a base quote for all ages between 18 - 25', function() {
+        it('calculate a quote based on lowest age: 18-25', function() {
+            let age = 20;
+            let answers = {q1: false, q2:1 , q3: false, q4: 2, q5: false};
+            assert.equal(insuranceCalculator(age,answers), 150);
+        });
+    });
+});
