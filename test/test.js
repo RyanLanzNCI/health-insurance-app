7e867fbaf46f42c1fab9d3021a264b2eed7b4c4d
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-04-10 14:22:30 +0100

    test first ci #14

--- test/test.js
+++ test/test.js
@@ -6,7 +6,7 @@ describe('insuranceCalculator', function() {
         it('calculate a quote based on lowest age: 18-25', function() {
             let age = 20;
             let answers = {q1: false, q2:1 , q3: false, q4: 2, q5: false};
-            assert.equal(calculate(age,answers), 150);
+            assert.equal(calculate(age,answers), 200);
         });
     });
 });
