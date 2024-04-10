8b42927626b46816158f6cb0175905518ddaeded
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-04-10 15:01:19 +0100

    tests for base quote for different age grousp

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 var {calculate} = require("../modules/insuranceCalculator")
 
+//Tests for ages 18-25
 describe('insuranceCalculator', function() {
     describe('Creates a base quote for all ages between 18 - 25', function() {
         it('calculate a quote based on lowest age: 18-25', function() {
@@ -10,3 +11,26 @@ describe('insuranceCalculator', function() {
         });
     });
 });
+
+//Tests for ages 26-40
+describe('insuranceCalculator', function() {
+    describe('Calculates base quote for ages 26-40', function() {
+        it('should calculate a base quote for age 30', function() {
+            let age = 30;
+            let answers = {q1: false, q2: 3, q3: false, q4: 5, q5: false}; 
+            assert.equal(calculate(age, answers), 130); 
+        });
+    });
+});
+
+//Tests for 40+
+describe('insuranceCalculator', function() {
+    describe('Calculates base quote for ages above 40', function() {
+        it('should calculate a base quote for age 45', function() {
+            let age = 45;
+            let answers = {q1: false, q2: 3, q3: false, q4: 5, q5: false}; // No extra costs
+            assert.equal(calculate(age, answers), 170); // 100 + 70 for age
+        });
+    });
+});
+
