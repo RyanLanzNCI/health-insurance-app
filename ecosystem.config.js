c5ff7c1575690435b20e0cdf97f22a33f1ee0e95
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-04-11 21:42:02 +0100

    manual deployment #6 added ecosystem.config.js file #2

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+//https://pm2.keymetrics.io/docs/usage/application-declaration/
+
+module.exports = {
+    apps : [{
+      name: 'health-app',
+      script: 'bin/www', 
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }]
+  };
+  
