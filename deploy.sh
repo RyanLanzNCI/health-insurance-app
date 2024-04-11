1af749c91cef82e969d34af42fc4190ec287489f
ryanlanzstu <135455767+ryanlanzstu@users.noreply.github.com>
2024-04-11 13:16:39 +0100

    cd deployment #9

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 18.17.0
+nvm use 18.17.0
 pm2 stop insurance_app
 cd health-insurance-app/
 npm install
-pm2 start .bin/www --name insurance_app
+pm2 start health-insurance-app-master/bin/www --name insurance_app
